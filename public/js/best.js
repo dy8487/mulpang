97c9cc424a7ca0fc26449561d15700b0f404fef8
dy.kwak <phnewma@hotmaiil.com>
2013-11-22 11:26:22 +0900

    막대그래프

--- public/js/best.js
+++ public/js/best.js
@@ -28,6 +28,36 @@ $(function(){
 // 판매순 그래프를 그린다.(Canvas)
 function drawSaleGraph(data){
 	
+	var context = $("#graph_by_sale")[0].getContext("2d");
+		
+	// x, y 축 그리기
+	context.beginPath();
+	context.moveTo(70,10);
+	context.lineTo(70, 230);
+	context.lineTo(470, 230);
+	context.stroke();
+	
+	// 막대 그래프 그리기
+	var r = 210 / data[0].buyQuantity; //높이 비율
+	var barW = 50;	//막대기 가로크기
+	var gap = 25;	//막대기 간격
+	$.each(data, function(i){
+		// 막대 그래프 그리기
+		var barH = this.buyQuantity * r;
+		
+		var x = (barW + gap) * i + gap + 60;		
+		var y = 230 - barH;
+				
+		context.fillStyle = "rgba(186, 68, 10, 0."+(7-i)+")";
+		context.fillRect(x, y, barW, barH);
+		
+		//텍스트
+		context.font = "12px '돋움, dotum, 굴림, gulim, sans-serif'";
+		context.fillStyle = "black";
+		context.textAlign = "center";
+		context.fillText(this.couponName, x + barW/2, 246);
+		context.fillText(this.buyQuantity, x + barW/2, y-5);
+	});
 }
 
 
