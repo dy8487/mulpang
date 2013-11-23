0b0ed2c172b889d97ca89bfdac473f90fa577dde
dy.kwak <phnewma@hotmail.com>
2013-11-23 15:04:26 +0900

    로그인

--- public/js/common_login.js
+++ public/js/common_login.js
@@ -2,10 +2,32 @@
 
 $(function(){
 	
+	$("#member_info > form").submit(login);
+	
 });
 
 // 로그인
 function login(){	
+	
+	var params = $(this).serialize();
+		
+		$.ajax({
+			url: "request",
+			dataType: "json",
+			type: "post",
+			data: params,
+			success: function(data){
+				
+				if(data.err){
+					alert(data.msg);
+				}else{					
+					window.location.href = "/";
+				}
+							
+			}
+		});
+		
+	return false; //submit 동작 중지
 	
 }
 
