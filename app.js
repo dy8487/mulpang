1235ef9f025434864fca11836a813f2d77f3df50
dy.kwak <phnewma@hotmail.com>
2013-11-23 10:50:23 +0900

    쿠키 와 세션 미들웨어

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ var express = require('express')
 
 var app = express();
 
+/* 
+ * Express (Node.js의 웹 프레임 워크) 에서의 Middleware 개념은 
+ * 쉽게 설명하자면 Java 에서의 Filter 와 비슷한 개념입니다. 
+ * 자주 쓰이고 통념적으로 쓰이는 행위들을 미리 걸러주는 역할을 한다고 
+ * 볼 수 있습니다.
+ * http://crazia.tistory.com/entry/Express-Nodejs-%EC%9D%98-%EC%9B%B9%ED%94%84%EB%A0%88%EC%9E%84%EC%9B%8C%ED%81%AC-%EC%9D%B5%EC%8A%A4%ED%94%84%EB%A0%88%EC%8A%A4%EC%97%90%EC%84%9C%EC%9D%98-%EB%AF%B8%EB%93%A4%EC%9B%A8%EC%96%B4-%EB%A7%8C%EB%93%A4%EA%B8%B0
+ * 
+ * */  
+
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
@@ -19,6 +28,12 @@ app.locals.layout = true;
 app.use(express.favicon());
 //app.use(express.logger('dev'));
 app.use(express.bodyParser());
+
+//쿠키 사용
+app.use(express.cookieParser());
+//세션 사용
+app.use(express.session({cookie: {maxAge: 1000*60*30}}));
+
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
